57592060d81e1851d0b8f38dfa7108dd928403ca
rkawachi <rafaelkawachi@gmail.com>
2017-06-25 17:27:06 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,14 +1,14 @@
 function carregahome() {
-document.getElementById(&quot;contentpage&quot;).innerHTML = &quot;<div class='cel g6' onclick='carregando(); carregaradios()'>RÁDIOS</div><div class='cel g6' onclick='carregando(); carregaestacoes()'>ESTAÇÕES</div>&quot;;
+document.getElementById("contentpage").innerHTML = "<div class='cel g6' onclick='carregando(); carregaradios()'>RÁDIOS</div><div class='cel g6' onclick='carregando(); carregaestacoes()'>ESTAÇÕES</div>";
 carregandoout();
 };
 
 function carregaradios() {
-document.getElementById(&quot;contentpage&quot;).innerHTML = &quot;<a href='http://radio.redekaz.com/2017/06/rkz-webradio.html' onclick='carregando()'><div>RÁDIO KAZ</div></a><br/><a href='http://radio.redekaz.com/2017/03/107FM.html' onclick='carregando()'><div>99FM</div></a>&quot;;
+document.getElementById("contentpage").innerHTML = "<a href='http://radio.redekaz.com/2017/06/rkz-webradio.html' onclick='carregando()'><div>RÁDIO KAZ</div></a><br/><a href='http://radio.redekaz.com/2017/03/107FM.html' onclick='carregando()'><div>99FM</div></a>";
 carregandoout();
 };
 
 function carregaestacoes() {
-document.getElementById(&quot;contentpage&quot;).innerHTML = &quot;<a href='http://radio.redekaz.com/2017/06/rkz-webradio.html' onclick='carregando()'><div>RÁDIO KAZ</div></a><br/><a href='http://radio.redekaz.com/2017/03/107FM.html' onclick='carregando()'><div>99FM</div></a>&quot;;
+document.getElementById("contentpage").innerHTML = "<a href='http://radio.redekaz.com/2017/06/rkz-webradio.html' onclick='carregando()'><div>RÁDIO KAZ</div></a><br/><a href='http://radio.redekaz.com/2017/03/107FM.html' onclick='carregando()'><div>99FM</div></a>";
 carregandoout();
 };
