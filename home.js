389bbb1f026582e968682243edac6afe4af769c8
rkawachi <rafaelkawachi@gmail.com>
2017-06-26 00:48:01 -0300

    Update home.js

--- home.js
+++ home.js
@@ -1,5 +1,4 @@
 function() {
-carregando();
 document.getElementById("contentpage").innerHTML = "<div class='cel g6'><span>Luis Fonsi feat. Daddy Yankee & Justin Bieber</span><h1>Despacito</h1></div><div class='cel g6'><span>Anitta</span><h1>Paradinha</h1></div>";
 carregandoout();
 };
