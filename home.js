d1caa162021860eaa0bbb388962867ee51b13ecc
rkawachi <rafaelkawachi@gmail.com>
2017-06-26 00:40:19 -0300

    Create home.js

---
+++ home.js
@@ -0,0 +1,5 @@
+function() {
+carregando();
+document.getElementById("contentpage").innerHTML = "<div class='cel g6'><span>Luis Fonsi feat. Daddy Yankee & Justin Bieber</span><h1>Despacito</h1></div><div class='cel g6'><span>Anitta</span><h1>Paradinha</h1></div>";
+carregandoout();
+};
