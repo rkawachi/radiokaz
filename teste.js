9d999694417fcda996c3af6715fe1ac2d4e9f55e
rkawachi <rafaelkawachi@gmail.com>
2018-06-08 09:32:49 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,14 +1,4 @@
 function carregahome() {
-document.getElementById("contentpage").innerHTML = "<div class='cel g6' onclick='carregando(); include('https://rkawachi.github.io/radiokaz/home.js');'>RÁDIOS</div><div class='cel g6' onclick='carregando(); carregaestacoes()'>ESTAÇÕES</div>";
-carregandoout();
-};
-
-function carregaradios() {
-document.getElementById("contentpage").innerHTML = "<a href='http://radio.redekaz.com/2017/06/rkz-webradio.html' onclick='carregando()'><div>RÁDIO KAZ</div></a><br/><a href='http://radio.redekaz.com/2017/03/107FM.html' onclick='carregando()'><div>99FM</div></a>";
-carregandoout();
-};
-
-function carregaestacoes() {
-document.getElementById("contentpage").innerHTML = "<a href='http://radio.redekaz.com/2017/06/rkz-webradio.html' onclick='carregando()'><div>RÁDIO KAZ</div></a><br/><a href='http://radio.redekaz.com/2017/03/107FM.html' onclick='carregando()'><div>99FM</div></a>";
+document.getElementById("contentpage").innerHTML = "<div class='cel g6' onclick='carregando();include('https://rkawachi.github.io/radiokaz/home.js');'>RÁDIOS</div><div class='cel g6' onclick='carregando(); carregaestacoes()'>ESTAÇÕES</div><br/>";
 carregandoout();
 };
